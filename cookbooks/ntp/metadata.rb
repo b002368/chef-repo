321490b4b333e651773dac9ab74ea610e6bd21f8
mike hsieh <mike.hsieh@macys.com>
2016-12-09 11:05:56 -0500

    add ntp cookbook

---
+++ cookbooks/ntp/metadata.rb
@@ -0,0 +1,16 @@
+name 'ntp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures my_cookbook'
+long_description 'Installs/Configures my_cookbook'
+version '1.6.9'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/my_cookbook/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/my_cookbook' if respond_to?(:source_url)
+#
